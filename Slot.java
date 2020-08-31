49382cc6c6eb1cdcedf8b58e4c80674cd2796dc3
Jyy-21 <70053110+Jyy-21@users.noreply.github.com>
2020-08-31 16:50:14 +0800

    Update Slot.java

--- Slot.java
+++ Slot.java
@@ -23,4 +23,24 @@ public class Slot{
     public Piece getPiece(){
         return this.piece;
     }
+    
+    public int getX()
+    {
+        return this.x;
+    }
+    
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public int getY()
+    {
+        return this.y;
+    }
+    
+    public void setY(int y) 
+    { 
+        this.y = y; 
+    } 
 }
