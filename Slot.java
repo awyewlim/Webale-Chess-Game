96947854dcace458c1d31186b3336de4a8fb3695
Aw Yew Lim <yewlim00@gmail.com>
2020-09-01 09:25:24 +0800

    Update Slot.java

--- Slot.java
+++ Slot.java
@@ -20,7 +20,23 @@ public class Slot{
         this.piece = piece;
     }
     
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
     public Piece getPiece(){
         return this.piece;
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
     }
 }
