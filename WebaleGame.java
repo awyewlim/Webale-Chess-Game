02f411f2c3ba85a9102bd7493ceeaae26b680740
Aw Yew Lim <yewlim00@gmail.com>
2020-09-03 20:25:22 +0800

    Update WebaleGame.java

--- WebaleGame.java
+++ WebaleGame.java
@@ -5,12 +5,15 @@ public class WebaleGame{
     Player player1;
     Player player2;
     ArrayList<Player> playerList = new ArrayList<Player>();
-    
+
     private static Piece queue = null;
     private static Slot temp = null;
     private static int playerTurn = 0;
     private static boolean hasWinner;
-  
+    private static boolean canMove = false, reachedEnd = false ;
+    private static String type;
+    private static  int fromX,fromY, toX, toY, x, y;
+    
     WebaleGame(){
         chessboard = new ChessBoard();
         player1 = new Player("B");
@@ -25,7 +28,7 @@ public class WebaleGame{
         playerTurn = 0;
         hasWinner = false;
     }
-    
+
     public void pieceSetup(){      
         String[] arrangement1 = {"Plus","Triangle","Chevron","Sun","Chevron","Triangle","Plus"};
         String arrangement2 = "Arrow";
@@ -48,51 +51,95 @@ public class WebaleGame{
             }
         }
     }
-    
+
     public boolean move(Slot slot){
         // if clicked button has piece
-        if(slot.getPiece() != null && movable(slot)){
+        if(slot.getPiece() != null && movable(slot))
+        {
             //if queue is empty
-            if(queue == null){
+            if(queue == null)
+            {
+                type = slot.getPiece().getPieceName();
+                fromX = slot.getX();
+                fromY = slot.getY();
                 queue = slot.getPiece();
                 temp = slot;
             }
             //if queue is occupied
-            else{
-                if(!queue.getPlayer().equals(slot.getPiece().getPlayer())){
+            else
+            {
+                if(!queue.getPlayer().equals(slot.getPiece().getPlayer()) && canMove)
+                {
                     temp.setPiece(null);
                     slot.setPiece(queue);
                     queue = null;
                     temp = null;
                     playerTurn++;
                     return true;
+
                 }
                 queue = null;
                 temp = null;
             }
         }
         //if clicked button has no piece
-        else{
-            if(temp != null){
-                slot.setPiece(queue);
-                queue = null;
-                temp.setPiece(null);
-                temp = null;
-                playerTurn++;
-                return true;
+        else
+        {
+            if(temp != null)
+            {
+                toX = slot.getX();
+                toY = slot.getY();
+                canMove = validMove(type,fromX,fromY,toX,toY);
+                if(canMove)
+                {
+                    slot.setPiece(queue);
+                    queue = null;
+                    temp.setPiece(null);
+                    temp = null;
+                    playerTurn++;
+                    return true;
+                }
             }
+            queue = null;
             temp = null;
         }
         return false;
     }
-    
+
     public boolean movable(Slot slot){
         if(slot.getPiece().getPlayer().equals(getPlayerTurn())){
             return true;
         }
         return false;
     }
-    
+
+    public boolean validMove(String type, int fromX, int fromY, int toX, int toY)
+    {
+        if(type.equals("Arrow"))
+        {
+            if(fromY == toY)
+            {
+                x = fromX - toX;
+                y = fromY - toY;
+                if(reachedEnd)
+                {
+                   
+                }
+                else
+                {
+                    if(x == 1 || (x == 2 && chessboard.getSlot(fromX - 1,fromY).getPiece() == null)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        else if (type.equals("Plus")){
+            return true;
+        }
+        return false;
+    }
+
     public String getWinner(){
         int numOfSun = 0;
         String winner = null;
@@ -113,7 +160,7 @@ public class WebaleGame{
         }
         return winner;        
     }
-    
+
     public Player getPlayerTurn(){
         if(hasWinner){
             return playerList.get((playerTurn - 1) % 2);
@@ -122,5 +169,5 @@ public class WebaleGame{
             return playerList.get(playerTurn % 2);
         }
     }
-   
+
 }
